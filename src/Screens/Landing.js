7697cd61125a28a50ea76987582eee474dc5c034
Rahul Dahal <rdaahal@gmail.com>
2022-01-09 15:16:31 +0545

    refactor: use useField hook on signIn form

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -1,8 +1,12 @@
 import react from "react";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
+import useField from "../hooks/useField";
 
 export default function Landing() {
+  const [emailRef, EmailField] = useField("Email Address");
+  const [passwordRef, PasswordField] = useField("Password");
+
   return (
     <main className="container m-auto h-screen flex items-center justify-around">
       <section className="">
@@ -14,24 +18,8 @@ export default function Landing() {
           frontend.
         </p>
         <form className="mt-12">
-          <label htmlFor="email" className="block">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            className="border focus:outline-green-500 w-full h-10 px-3 mb-5 rounded-md"
-            placeholder="ram@domain.com"
-          />
-          <label htmlFor="password" className="block">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            className="border focus:outline-green-500 w-full h-10 px-3 mb-5 rounded-md"
-            placeholder="password"
-          />
+          <EmailField type="email" placeholder="ram@domain.com" />
+          <PasswordField type="password" placeholder="password" />
           <small className="block">Don't have an account? Sign Up</small>
           <Button
             modifier="mt-5 bg-green-500 hover:bg-green-700 shadow-xl text-white"

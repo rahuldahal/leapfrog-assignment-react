e4799ffc6e192928e58324e46263e1edb4651e17
Rahul Dahal <rdaahal@gmail.com>
2022-01-09 15:17:33 +0545

    fix: typo on React import

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import useField from "../hooks/useField";

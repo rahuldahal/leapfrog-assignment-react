8f58640719411e0ce6523e8df6046a66ec00b939
Rahul Dahal <rdaahal@gmail.com>
2022-01-11 19:36:18 +0545

    wip: wrap App with the StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
 import Landing from "./Screens/Landing";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App p-4 lg:p-0 bg-green-50 text-gray-700">
-      <Landing />
-    </div>
+    <StoreProvider store={store}>
+      <div className="App p-4 lg:p-0 bg-green-50 text-gray-700">
+        <Landing />
+      </div>
+    </StoreProvider>
   );
 }
 

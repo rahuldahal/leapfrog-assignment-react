f1d56656a4f851c4c85610cfa4e7cc7cd92e29d0
Rahul Dahal <rdaahal@gmail.com>
2022-01-12 21:44:29 +0545

    feat: initialize reduxThunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import reduxThunk from "redux-thunk";
 import authReducer from "./auth/reducer";
 
-export default createStore(authReducer);
+export default createStore(authReducer, applyMiddleware(reduxThunk));

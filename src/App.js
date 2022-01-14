c89d161d476b40fe4e7c6932be0315f15d9c611a
Rahul Dahal <rdaahal@gmail.com>
2022-01-14 13:09:16 +0545

    feat: initialize react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import Landing from "./Screens/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider as StoreProvider } from "react-redux";
 import store from "./redux/store";
+import Landing from "./Screens/Landing";
+import Contacts from "./Screens/Contacts";
 
 function App() {
   return (
-    <StoreProvider store={store}>
-      <div className="App p-4 lg:p-0 bg-green-50 text-gray-700">
-        <Landing />
-      </div>
-    </StoreProvider>
+    <Router>
+      <StoreProvider store={store}>
+        <div className="App p-4 lg:p-0 bg-green-50 text-gray-700">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </div>
+      </StoreProvider>
+    </Router>
   );
 }
 
---
+++ src/Screens/Contacts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "../components/Heading";
+
+export default function Contacts({ contatcs }) {
+  return (
+    <main>
+      <Heading level="3">Contacts</Heading>
+    </main>
+  );
+}

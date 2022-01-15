06e0195bed8cdedc9d586af5011d70c3477434ac
Rahul Dahal <rdaahal@gmail.com>
2022-01-15 14:28:08 +0545

    fix: infinite render loop

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -2,6 +2,6 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function ProtectedRoutes() {
-  const { isAuthenticated } = useSelector((state) => state);
+  const { isAuthenticated } = useSelector((state) => state.auth);
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 }

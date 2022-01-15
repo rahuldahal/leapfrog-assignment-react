164f0f2a357aaa358be78861934424e2d238d110
Rahul Dahal <rdaahal@gmail.com>
2022-01-15 18:42:53 +0545

    feat: update a contact

--- src/components/ContactsTable.js
+++ src/components/ContactsTable.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import Modal from "../Modal";
 import { deleteContactRequest } from "../redux/contacts/actions";
+import AddContactForm from "./AddContactForm";
 import Button from "./Button";
 
 export default function ContactsTable({ headings, contacts }) {
   const dispatch = useDispatch();
+  const [updateContactModalData, setUpdateContactModalData] = useState(null);
+
+  function hideModalOnEscapePress(e) {
+    if (e.key === "Escape") {
+      setUpdateContactModalData(null);
+    }
+  }
+
+  useEffect(() => {
+    if (updateContactModalData) {
+      window.addEventListener("keyup", hideModalOnEscapePress);
+    } else {
+      window.removeEventListener("keyup", hideModalOnEscapePress);
+    }
+  }, [updateContactModalData]);
+
   function handleDelete(_id) {
     const accessToken = localStorage.getItem("accessToken");
     const parsedToken = JSON.parse(accessToken);
@@ -19,49 +37,69 @@ export default function ContactsTable({ headings, contacts }) {
     );
   }
   return (
-    <table className="min-w-full leading-normal">
-      <thead>
-        <tr>
-          {headings.map((heading, index) => (
-            <TableHeading key={index} label={heading} />
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact) => {
-          const { _id, name, phone, photograph } = contact;
-          return (
-            <tr key={_id}>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0 w-10 h-10">
-                    <img
-                      className="w-full h-full rounded-full"
-                      src={photograph}
-                      alt=""
-                    />
+    <>
+      <table className="min-w-full leading-normal">
+        <thead>
+          <tr>
+            {headings.map((heading, index) => (
+              <TableHeading key={index} label={heading} />
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            const { _id, name, phone, photograph } = contact;
+            return (
+              <tr key={_id}>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 w-10 h-10">
+                      <img
+                        className="w-full h-full rounded-full"
+                        src={photograph}
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-gray-900 whitespace-no-wrap">{name}</p>
+                    </div>
                   </div>
-                  <div className="ml-3">
-                    <p className="text-gray-900 whitespace-no-wrap">{name}</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                <p className="text-gray-900 whitespace-no-wrap">{phone}</p>
-              </td>
-              <td className="flex justify-between px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                <Button modifier="bg-blue-400 text-white">Edit</Button>
-                <Button
-                  modifier="bg-red-400 text-white"
-                  onClick={() => handleDelete(_id)}
-                >
-                  Delete
-                </Button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <p className="text-gray-900 whitespace-no-wrap">{phone}</p>
+                </td>
+                <td className="flex justify-between px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <Button
+                    modifier="bg-blue-400 text-white"
+                    onClick={() =>
+                      setUpdateContactModalData({
+                        _id,
+                        name,
+                        phone,
+                        photograph,
+                      })
+                    }
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    modifier="bg-red-400 text-white"
+                    onClick={() => handleDelete(_id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {updateContactModalData ? (
+        <Modal>
+          <AddContactForm data={updateContactModalData} />
+        </Modal>
+      ) : null}
+    </>
   );
 }

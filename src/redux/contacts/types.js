b2d47573105f7e936447ac3cd544bab62284295b
Rahul Dahal <rdaahal@gmail.com>
2022-01-15 16:36:49 +0545

    refactor: add 'new' action type

--- src/redux/contacts/types.js
+++ src/redux/contacts/types.js
@@ -3,6 +3,10 @@ const contactsTypes = {
     success: "GET_ALL_SUCCESS",
     failure: "GET_ALL_FAILURE",
   },
+  newContact: {
+    success: "NEW_CONTACT_SUCCESS",
+    failure: "NEW_CONTACT_FAILURE",
+  },
   update: {
     success: "UPDATE_SUCCESS",
     failure: "UPDATE_FAILURE",

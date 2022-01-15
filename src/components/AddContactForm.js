828c013dd50b763858d691261dd3026cd3b358ea
Rahul Dahal <rdaahal@gmail.com>
2022-01-15 18:42:28 +0545

    refactor: handle create/update w.r.t parameter

--- src/components/AddContactForm.js
+++ src/components/AddContactForm.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import useField from "../hooks/useField";
-import { newContactRequest } from "../redux/contacts/actions";
+import {
+  newContactRequest,
+  updateContactRequest,
+} from "../redux/contacts/actions";
 import Button from "./Button";
 
-export default function AddContactForm() {
+export default function AddContactForm({ data = {} }) {
+  const { _id, name: initialName, phone: initialPhone } = data;
   const [nameRef, NameField] = useField("Full Name");
   const [phoneRef, PhoneField] = useField("Phone Number");
   const [photographRef, PhotographField] = useField("Photograph URL");
@@ -18,7 +22,18 @@ export default function AddContactForm() {
     console.log({ name, phone, photograph });
     const accessToken = localStorage.getItem("accessToken");
     const parsedToken = JSON.parse(accessToken);
-    dispatch(
+    if (Object.keys(data).length > 0) {
+      return dispatch(
+        updateContactRequest({
+          _id,
+          name,
+          phone,
+          photograph,
+          token: parsedToken,
+        })
+      );
+    }
+    return dispatch(
       newContactRequest({ name, phone, photograph, token: parsedToken })
     );
   }
@@ -27,11 +42,15 @@ export default function AddContactForm() {
       className="w-3/5 m-auto bg-white p-4 rounded-md text-gray-700"
       onSubmit={handleFormSubmit}
     >
-      <NameField type="text" placeholder="John Doe" />
-      <PhoneField type="number" placeholder="10 digits phone number" />
-      <PhotographField type="file" />
+      <NameField type="text" placeholder="John Doe" value={initialName} />
+      <PhoneField
+        type="number"
+        placeholder="10 digits phone number"
+        value={initialPhone}
+      />
+      <PhotographField type="file" value="" />
       <Button type="submit" modifier="bg-green-500 text-white">
-        Add
+        {data ? "Update" : "Add"}
       </Button>
     </form>
   );

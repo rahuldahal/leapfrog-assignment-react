9089f539fc343240cfc79f07c89cd6a2fb116590
Rahul Dahal <rdaahal@gmail.com>
2022-01-16 16:30:05 +0545

    feat: FlashMessage component

---
+++ src/components/FlashMessage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function FlashMessage({ type, message }) {
+  const typeColorMap = {
+    success: ["bg-green-500", "text-gray-700"],
+    warning: ["bg-yellow-500", "text-gray-700"],
+    error: ["bg-red-500", "text-white"],
+  };
+  return (
+    <div
+      className={`${typeColorMap[type].join(
+        " "
+      )} p-4 rounded-lg absolute bottom-8 left-4 animate-bounce`}
+    >
+      <p>{message}</p>
+    </div>
+  );
+}

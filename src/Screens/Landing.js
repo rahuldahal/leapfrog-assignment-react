618b5ef0322f97b76f8f125d60e394bcc8b2ea29
Rahul Dahal <rdaahal@gmail.com>
2022-01-17 18:24:13 +0545

    feat: add flashMessage on Landing

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -5,14 +5,23 @@ import useField from "../hooks/useField";
 import { useSelector, useDispatch } from "react-redux";
 import { signInRequest, signUpRequest } from "../redux/auth/actions";
 import { useNavigate } from "react-router-dom";
+import Modal from "../Modal";
+import FlashMessage from "../components/FlashMessage";
 
 export default function Landing({ history }) {
   const [formType, setFormType] = useState("signIn");
   const [emailRef, EmailField] = useField("Email Address");
   const [passwordRef, PasswordField] = useField("Password");
   const { message, error } = useSelector((state) => state.auth);
+  const [flashMessage, setFlashMessage] = useState(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (flashMessage) {
+      setTimeout(() => setFlashMessage(null), 3000);
+    }
+  }, [flashMessage]);
 
   useEffect(() => {
     if (message) {
@@ -23,6 +32,10 @@ export default function Landing({ history }) {
     }
     if (error) {
       console.log(error);
+      setFlashMessage({
+        type: "error",
+        message: error,
+      });
     }
   }, [message, error, navigate]);
 
@@ -57,35 +70,46 @@ export default function Landing({ history }) {
   }
 
   return (
-    <main className="container m-auto h-screen flex items-center justify-around">
-      <section className="">
-        <Heading level="1">Wanna Leap?</Heading>
-        <p className="max-w-sm">
-          Leapfrog Technology, Inc. assignment that consumes{" "}
-          <strong className="text-green-500">node.js' CRUD API </strong>
-          with <strong className="text-green-500">React</strong> in the
-          frontend.
-        </p>
-        <form className="mt-12" onSubmit={handleFormSubmit}>
-          <EmailField type="email" placeholder="ram@domain.com" />
-          <PasswordField type="password" placeholder="password" />
-          <FormTypeToggler />
-          <Button
-            modifier="mt-5 bg-green-500 hover:bg-green-700 shadow-xl text-white"
-            type="submit"
-          >
-            {formType === "signIn" ? "Sign In" : "Sign Up"}
-          </Button>
-        </form>
-      </section>
+    <>
+      <main className="container m-auto h-screen flex items-center justify-around">
+        <section className="">
+          <Heading level="1">Wanna Leap?</Heading>
+          <p className="max-w-sm">
+            Leapfrog Technology, Inc. assignment that consumes{" "}
+            <strong className="text-green-500">node.js' CRUD API </strong>
+            with <strong className="text-green-500">React</strong> in the
+            frontend.
+          </p>
+          <form className="mt-12" onSubmit={handleFormSubmit}>
+            <EmailField type="email" placeholder="ram@domain.com" />
+            <PasswordField type="password" placeholder="password" />
+            <FormTypeToggler />
+            <Button
+              modifier="mt-5 bg-green-500 hover:bg-green-700 shadow-xl text-white"
+              type="submit"
+            >
+              {formType === "signIn" ? "Sign In" : "Sign Up"}
+            </Button>
+          </form>
+        </section>
 
-      <aside className="hidden lg:block relative w-96 h-max after:block after:rounded-md after:border-b-8 after:border-r-8 after:border-green-500 after:absolute after:-bottom-2 after:-right-2 after:w-24 after:h-full">
-        <img
-          className="w-auto h-auto rounded-md rounded-br-none"
-          src="/leap-silhouette.jpg"
-          alt=""
-        />
-      </aside>
-    </main>
+        <aside className="hidden lg:block relative w-96 h-max after:block after:rounded-md after:border-b-8 after:border-r-8 after:border-green-500 after:absolute after:-bottom-2 after:-right-2 after:w-24 after:h-full">
+          <img
+            className="w-auto h-auto rounded-md rounded-br-none"
+            src="/leap-silhouette.jpg"
+            alt=""
+          />
+        </aside>
+      </main>
+
+      {flashMessage ? (
+        <Modal transparent>
+          <FlashMessage
+            type={flashMessage.type}
+            message={flashMessage.message}
+          />
+        </Modal>
+      ) : null}
+    </>
   );
 }

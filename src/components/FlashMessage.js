a77079fa3898aafbfe992904680ffc89dfcd2a5c
Rahul Dahal <rdaahal@gmail.com>
2022-01-20 10:51:05 +0545

    fix: text color on success message

--- src/components/FlashMessage.js
+++ src/components/FlashMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FlashMessage({ type, message }) {
   const typeColorMap = {
-    success: ["bg-green-500", "text-gray-700"],
+    success: ["bg-green-500", "text-white"],
     warning: ["bg-yellow-500", "text-gray-700"],
     error: ["bg-red-500", "text-white"],
   };
